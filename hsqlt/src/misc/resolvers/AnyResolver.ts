c38650aab08af027731927b30c4216636793ccd2
Atreya Bain <christophervrose@gmail.com>
2021-03-24 22:45:09 +0530

    [drop] readers and resolvers

--- hsqlt/src/misc/resolvers/AnyResolver.ts
+++
@@ -1,9 +0,0 @@
-import { AnyModule } from '../../ast/data/AnyModule';
-import { QualifiedIdentifier } from '../ast';
-import { IResolver } from './IResolver';
-
-export class AnyResolver implements IResolver {
-    getResult(z: QualifiedIdentifier) {
-        return new AnyModule();
-    }
-}
--- hsqlt/src/misc/resolvers/IResolver.ts
+++
@@ -1,6 +0,0 @@
-import { DataType } from '../../ast/data/base/DataType';
-import { QualifiedIdentifier } from '../ast';
-
-export interface IResolver {
-    getResult(q: QualifiedIdentifier): DataType;
-}
