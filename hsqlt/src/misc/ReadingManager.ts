fd21c6d13fb51bd48aa6b32b519790a1e89f053c
Atreya Bain <christophervrose@gmail.com>
2021-03-24 22:44:20 +0530

    [add] read manager

---
+++ hsqlt/src/misc/ReadingManager.ts
@@ -0,0 +1,111 @@
+import { QualifiedIdentifier } from './ast/QualifiedIdentifier';
+import fs from 'fs';
+import path, { sep } from 'path';
+import { Module } from '../ast/data/Module';
+import { ErrorManager } from './error/Error';
+import { AnyModule } from '../ast/data/AnyModule';
+export enum FILETYPE {
+    ECL,
+    DECL,
+    HSQL,
+}
+
+/**
+ * Read and resolve types for files
+ */
+export class ReadingManager {
+    /**
+     *
+     * @param errorListener Error listener
+     * @param memFileMap Override for files
+     * @param baseLoc Base location to read from
+     */
+    constructor(
+        protected errorListener: ErrorManager,
+        protected memFileMap: Map<string, string> = new Map<string, string>(),
+        protected baseLoc?: string
+    ) {}
+
+    /**
+     * resolve a
+     * @param q resolve this qualified identifier into face
+     * @returns the module that was resolved
+     */
+    resolve(q: QualifiedIdentifier): Module {
+        // const identifiers = q.qidentifier;
+        // let joinable = '.';
+
+        // for (const segment of identifiers) {
+        //     joinable = path.join(joinable, segment);
+        //     if (!fs.existsSync(joinable)) {
+        //         this.errorListener;
+        //     }
+        // }
+        // return joinable;
+
+        // FIXME actually resolve
+        return new AnyModule();
+    }
+
+    /**
+     * remove file map
+     * @param s source name
+     * @returns
+     */
+    removeMap(s: string) {
+        return this.memFileMap.delete(s);
+    }
+    /**
+     * Add file map cautiously
+     * @param s
+     * @param file
+     * @returns false if exists or could not set
+     */
+    addMap(s: string, file: string) {
+        if (this.memFileMap.has(s)) return false;
+        return this.memFileMap.set(s, file);
+    }
+    /**
+     * Set file map
+     * @param s
+     * @param file
+     * @returns
+     */
+    setMap(s: string, file: string) {
+        return this.memFileMap.set(s, file);
+    }
+
+    /**
+     * check if sourcefile exists in map
+     * @param s source name
+     * @returns
+     */
+    exists(s: string) {
+        return this.memFileMap.has(s);
+    }
+
+    entries() {
+        return this.memFileMap.entries();
+    }
+
+    /**
+     * read a file
+     * @param fileName
+     */
+    readSync(fileName: string) {
+        if (this.memFileMap.has(fileName)) return this.memFileMap.get(fileName);
+        return fs.readFileSync(fileName);
+    }
+
+    /**
+     * Asynchoronously read a file
+     * @param fileName
+     * @returns
+     */
+    async read(fileName: string): Promise<string> {
+        if (this.memFileMap.has(fileName)) return this.memFileMap.get(fileName)!;
+        const file = await fs.promises.readFile(fileName);
+        return file.toString();
+    }
+    discoverFileType(q: QualifiedIdentifier) {}
+}
