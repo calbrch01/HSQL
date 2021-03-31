62b175e670ab5a81280274e4a5c8351d5c0aa4b3
Atreya Bain <christophervrose@gmail.com>
2021-03-31 21:35:01 +0530

    [add] error severity enums

--- hsqlt/src/misc/error/Error.ts
+++ hsqlt/src/misc/error/Error.ts
@@ -1,21 +1,23 @@
 import { ANTLRErrorListener, BailErrorStrategy, RecognitionException, Recognizer, Token } from 'antlr4ts';
-// export enum ErrorType {
-//     SETUP,
-//     IO,
-//     SYNTAX,
-//     SEMANTIC,
-//     OTHER,
-//     /**
-//      * This should never occur
-//      */
-//     OTHERWORDLY,
-// }
+
+export enum ErrorSeverity {
+    INFO,
+    WARNING,
+    ERROR,
+}
+export enum ErrorType {
+    SETUP,
+    IO,
+    SYNTAX,
+    SEMANTIC,
+    OTHER,
+    /**
+     * This should never occur
+     */
+    OTHERWORDLY,
+}
 
 export enum ErrorMode {
-    /**
-     * Only stop on unrecoverable errors
-     */
-    STEAMROLL,
     /**
      * Stop with errors
      */
@@ -25,18 +27,15 @@ export enum ErrorMode {
      */
     PEDANTIC,
 }
-export interface TranslationError {
-    line: number;
-    charPositionInLine: number;
-    message: string;
-}
 
-export class SyntaxError implements TranslationError {
-    constructor(public line: number, public charPositionInLine: number, public message: string) {}
-}
-
-export class SemanticError implements TranslationError {
-    constructor(public line: number, public charPositionInLine: number, public message: string) {}
+export class TranslationError {
+    constructor(
+        public line: number,
+        public charPositionInLine: number,
+        public msg: string,
+        public severity: ErrorSeverity = ErrorSeverity.ERROR,
+        public type: ErrorType = ErrorType.SYNTAX
+    ) {}
 }
 
 export type LexerOrParserSymbol = number | Token;
@@ -54,7 +53,7 @@ export class ErrorManager {
         const errors = this._errors;
         const listener: ANTLRErrorListener<LexerOrParserSymbol> = {
             syntaxError(rec, offendingSymbol, line, charPositionInLine, msg, e) {
-                errors.push(new SyntaxError(line, charPositionInLine, msg));
+                errors.push(new TranslationError(line, charPositionInLine, msg));
             },
         };
         return listener;
