caba485f51e852c13faefc0533a17542e880d404
Christopher Rose <christophervrose@gmail.com>
2021-06-09 17:43:00 +0530

    [server] add diagnostics

---
+++ hsqlt-extension/server/src/Translation.ts
@@ -0,0 +1,70 @@
+import { Position, Range, TextDocuments, URI, Diagnostic } from 'vscode-languageserver';
+import { TextDocument, TextDocumentContentChangeEvent } from 'vscode-languageserver-textdocument';
+import { DocListener, FTDoc } from './TextDoc';
+import { eventChecks } from './TextUtils';
+import { fileURLToPath, pathToFileURL } from 'url';
+import { AST, ContexedTranslationError, TaskManager } from 'hsqlt';
+
+// TODO someday work with non-file:// functions
+export function validator(
+    document: FTDoc,
+    changes: TextDocumentContentChangeEvent[],
+    version: number,
+    documents: TextDocuments<TextDocument>
+) {
+    const changesInc = changes.filter(eventChecks.eventIsIncremental);
+    const allDocs = documents.all();
+
+    const mainFile = fileURLToPath(document.uri);
+
+    const disposable: [string, string][] = allDocs.map(e => [fileURLToPath(e.uri), e.getText()]);
+    const fileMap = new Map(disposable);
+
+    const tm = new TaskManager(mainFile, false, fileMap);
+    const results = tm.generateAST();
+    return {
+        ...results,
+        issues: tm.errorManager.issues,
+    };
+}
+
+/**
+ * Map HSQLT errors to LSP diagnostics
+ * @param issues
+ * @returns
+ */
+export function mapIssues(
+    issues: ContexedTranslationError[],
+    fileList: string[]
+): Map<URI, Diagnostic[]> {
+    const fileListMapped: [string, Diagnostic[]][] = fileList.map(e => [e, []]);
+    const mappedIssues = new Map<URI, Diagnostic[]>(fileListMapped);
+    for (const issue of issues) {
+        const fn = pathToFileURL(issue.ctx).toString();
+        const entry = mappedIssues.get(fn) ?? [];
+
+        const startPosition = Position.create(
+            (issue.line ?? 1) - 1,
+            (issue.charPositionInLine ?? 1) - 1
+        );
+
+        // FIXME use a range
+        entry.push({ message: issue.msg, range: Range.create(startPosition, startPosition) });
+        mappedIssues.set(fn, entry);
+    }
+
+    return mappedIssues;
+}
+
+/**
+ * Get the list of files the ASTs are generated for
+ * @param asts List of ASTs
+ * @returns
+ */
+export function getFileList(asts: Map<string, AST>): string[] {
+    const fileNameList: string[] = [];
+    for (const [fileName] of asts.entries()) {
+        fileNameList.push(pathToFileURL(fileName).toString());
+    }
+    return fileNameList;
+}
--- hsqlt-extension/server/src/index.ts
+++ hsqlt-extension/server/src/index.ts
@@ -1,14 +1,77 @@
 #!/usr/bin/env node
-import { createConnection, ProposedFeatures } from "vscode-languageserver";
-import { TextDocument } from "vscode-languageserver-textdocument";
-import { ErrorManager, ErrorMode, TaskManager } from "hsqlt";
+import {
+    createConnection,
+    ProposedFeatures,
+    TextDocumentPositionParams,
+    CompletionItem,
+    CompletionItemKind,
+    TextDocumentSyncKind,
+    TextDocuments,
+    Diagnostic,
+} from 'vscode-languageserver';
+import { TDocs, FTDoc } from './TextDoc';
+import { ErrorManager, ErrorMode, TaskManager } from 'hsqlt';
+import { eventChecks } from './TextUtils';
+
+import { fileURLToPath } from 'url';
+import { getFileList, mapIssues, validator } from './Translation';
+
 const connection = createConnection(ProposedFeatures.all);
 
-connection.onDidChangeWatchedFiles((change) => {
-    // Monitored files have change in VS Code
-    console.debug(change);
-    const x = ErrorMode.NORMAL;
-    connection.console.log("We received a file change event" + x);
+// text documents based on string based docs
+const tdocs = new TDocs(FTDoc, [
+    (d, c, v) => {
+        connection.console.log(`Incremental change on ${d.uri} ${JSON.stringify(c)}`);
+
+        const needDiagnostics = true;
+        if (needDiagnostics) {
+            // need to generate file map
+            console.log(fileURLToPath(d.uri));
+            const { asts, issues } = validator(d, c, v, documents);
+            connection.console.log(`I>Got diagnostics :${issues.length} issues`);
+
+            const fileList = getFileList(asts);
+
+            const mappedIssues = mapIssues(issues, fileList);
+            connection.console.log(`I>Mapped the diagnostics filewise ${mappedIssues.size}`);
+            // send these over
+            // FIXME merge the asts uri map with this one
+            const issuesList = [...mappedIssues];
+            issuesList.forEach(([uri, diagnostics]) => {
+                connection.sendDiagnostics({ uri, diagnostics });
+            });
+        }
+    },
+]);
+//
+const documents = new TextDocuments(tdocs);
+
+// documents.onDidChangeContent((e) => {
+//     // we will have to get diagnostics here, lets try
+// });
+
+documents.onDidOpen(e => {
+    connection.console.log(`Opened ${e.document.uri} - ${e.document.languageId}`);
 });
 
+documents.onDidClose(e => {
+    connection.console.log(`Closed ${e.document.uri} - ${e.document.languageId}`);
+});
+
+connection.onInitialize(params => {
+    const workspaceDirs = params.workspaceFolders;
+    return {
+        capabilities: {
+            textDocumentSync: {
+                openClose: true,
+                change: TextDocumentSyncKind.Incremental,
+                willSave: true,
+            },
+        },
+    };
+});
+
+//Document manager should tune into the connection
+documents.listen(connection);
+// and finally, listen for some input
 connection.listen();
