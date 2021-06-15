d17c655920369cf8ea96c3148b01c9ba37c89164
Christopher Rose <christophervrose@gmail.com>
2021-06-16 00:05:53 +0530

    [dev] circular dependency detector

--- hsqlt/src/test/index.ts
+++ hsqlt/src/test/index.ts
@@ -5,10 +5,10 @@
 
 import 'mocha';
 import { assert } from 'chai';
-import { exec } from 'child_process';
 import path from 'path';
 import os from 'os';
 import { Readable } from 'stream';
+import { execAndGetCode } from './lib';
 describe('Grammar generation tests', function () {
     //this test takes a bit of time
     this.timeout(10000);
@@ -45,43 +45,3 @@ describe('Grammar generation tests', function () {
         assert.strictEqual(returnCode, 0, 'Should have been valid grammar');
     });
 });
-
-type execResult = {
-    returnCode: number | null;
-    stdout: string;
-    stderr: string;
-};
-
-/**
- * Run a process and yield the return type
- */
-async function execAndGetCode(command: string) {
-    return new Promise<execResult>((res, rej) => {
-        /*
-         * Run this command and reject instantly if it fails.
-         * We care about its output & its exit code
-         */
-        const proc = exec(command, (err, _stdout, _stderr) => {
-            if (err) rej(err);
-        });
-        let stdout = '',
-            stderr = '';
-        proc.stdout?.on('data', chunk => (stdout += chunk.toString()));
-        proc.stderr?.on('data', chunk => (stderr += chunk.toString()));
-
-        /** If we error out, bail and reject */
-        proc.on('error', err => {
-            rej(err);
-        });
-
-        /** If we are done, exit */
-        proc.on('exit', returnCode => {
-            res({ returnCode, stdout, stderr });
-        });
-
-        /** If we close, exit */
-        proc.on('close', returnCode => {
-            res({ returnCode, stdout, stderr });
-        });
-    });
-}
